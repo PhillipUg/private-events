b4f2ba91b678f5407cc75b376015892ec75366bd
phillipug <phillipmusiime@gmail.com>
2020-06-02 14:08:15 +0300

    removed home controller and changed root route to events_path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'events/new'
   # get 'events/create'
   # get 'events/show'
-	root 'home#index'
+	root 'events#index'
   resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/login' => 'sessions#new'
