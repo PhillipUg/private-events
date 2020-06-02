7076e3ed0731a9b8d1cf80a44fe667f0739fbe7d
phillipug <phillipmusiime@gmail.com>
2020-06-02 11:41:37 +0300

    added notices in app layout and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-	  	redirect_to @user
+	  	redirect_to @user, notice: "User Successfully Created!"
 	  else
 	  	render :new
 	  end
