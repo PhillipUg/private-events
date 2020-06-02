d09c7302ebb8c4929cf55747682c397883ca11ab
phillipug <phillipmusiime@gmail.com>
2020-06-02 22:42:34 +0300

    added 'validate' helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
+
+	def authorize
+		redirect_to login_path, alert: 'You must be logged in to access this page.' if current_user.nil?
+	end
 end
