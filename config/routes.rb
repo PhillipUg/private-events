0243f3ef9ecfce4ad6e8d201b255a04cb99bd013
phillipug <phillipmusiime@gmail.com>
2020-06-02 11:40:57 +0300

    session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+	root 'home#index'
   resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
