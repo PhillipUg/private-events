1e04ce16294f3e2963e102c40c858243123c8ed2
phillipug <phillipmusiime@gmail.com>
2020-06-02 16:43:23 +0300

    created many-to-many :through associations btn models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
 	has_many :created_events, class_name: :Event, foreign_key: :creator_id
+
+	has_many :attendances, foreign_key: :attendee_id
+	has_many :attended_events, through: :attendances, source: :attended_event
 end
