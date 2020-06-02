29c2d9c06b11fed345c6ed1597d22328db2f6584
phillipug <phillipmusiime@gmail.com>
2020-06-02 14:10:54 +0300

    added create and show action to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,23 @@
 class EventsController < ApplicationController
   def index
+  	@events = Event.all
+  	@event = Event.new
   end
 
   def new
+  	@event = current_user.created_events.new
   end
 
   def create
+  	@event = current_user.created_events.build(params.require(:event).permit(:name))
+  	if @event.save
+	  	redirect_to @event, notice: "Event Successfully Created!"
+	  else
+	  	render :new
+	  end
   end
 
   def show
+  	@event = Event.find(params[:id])
   end
 end
