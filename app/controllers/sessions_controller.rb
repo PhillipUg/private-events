c8fe7d8db531d1825cd762b7780ea78bef44eedd
phillipug <phillipmusiime@gmail.com>
2020-06-02 11:38:58 +0300

    seesion controller and views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(username: params[:username])
+
+  	if user
+  		session[:user_id] = user.id
+  		redirect_to root_path, notice: "Successfully Logged In"
+  	else
+  		flash.now.alert = "Incorrect Username. try again."
+  		render :new
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to login_path, notice: "Logged out!"
+  end
+end
