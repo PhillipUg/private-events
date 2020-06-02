8e95ee6058ac84fe1b2328b33e93a4e410dc9298
phillipug <phillipmusiime@gmail.com>
2020-06-02 10:18:06 +0300

    removed email & password columns from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_054043) do
+ActiveRecord::Schema.define(version: 2020_06_02_062115) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
