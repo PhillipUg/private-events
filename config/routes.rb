688b5b7fd2a7f263b3a8a8de7a5fbaa14568eb8d
phillipug <phillipmusiime@gmail.com>
2020-06-02 12:36:29 +0300

    event controller, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  # get 'events/index'
+  # get 'events/new'
+  # get 'events/create'
+  # get 'events/show'
 	root 'home#index'
   resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
+  resources :events, only: [:index, :new, :create, :show]
 end
