33a11b4a747405b47e998603178ff6789df0086d
phillipug <phillipmusiime@gmail.com>
2020-06-03 17:35:31 +0300

    Added links for easy navigation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to login_path, notice: "Logged out!"
+  	redirect_to root_path, notice: "Logged out!"
   end
 end
