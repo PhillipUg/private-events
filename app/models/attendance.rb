f602ff6ccbc793678c0f2d6899b78a52b082b2b4
phillipug <phillipmusiime@gmail.com>
2020-06-03 17:33:22 +0300

    Added validation to weed out duplicate attendances

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,11 @@ class Attendance < ApplicationRecord
   belongs_to :attended_event, class_name: :Event
   belongs_to :attendee, class_name: :User
 
-  validates :attended_event, uniqueness: true
+  validates :attendee, uniqueness: true, if: :already_attended?
+
+  def already_attended?
+  	attended_event.attendees.include?(attendee)
+  end
 
   def self.join_event(user_id, event_id)
   	self.create(attendee_id: user_id, attended_event_id: event_id)
