2fbdff38a05821b05d1c8b74b62a8824caaebaa3
phillipug <phillipmusiime@gmail.com>
2020-06-03 16:22:26 +0300

    added past and upcoming helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 	helper_method :current_user
+	helper_method :previous_events
+	helper_method :upcoming_events
+	helper_method :upcoming
+	helper_method :past
 
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -7,5 +11,21 @@ class ApplicationController < ActionController::Base
 
 	def authorize
 		redirect_to login_path, alert: 'You must be logged in to access this page.' if current_user.nil?
+	end
+
+	def upcoming
+		Event.upcoming.all
+	end
+
+	def past
+		Event.past.all
+	end
+
+	def upcoming_events
+		@upcoming_events ||= current_user.attended_events.where("start_date > ?", Time.now)
+	end
+
+	def previous_events
+		@prev_events ||= current_user.attended_events.where("start_date < ?", Time.now)
 	end
 end
